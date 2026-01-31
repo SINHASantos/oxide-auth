SINHASantos/oxide-auth

commit 71dd0751f92c5784f696bff9645d8b88b5f481e8

clippy: resolve result_unit_err
Example warning resolved by this change:

warning: this returns a `Result<_, ()>`
  --> oxide-auth/src/primitives/issuer.rs:28:5
   |
28 |     fn refresh(&mut self, _refresh: &str, _grant: Grant) -> Result<RefreshedToken, ()>;
   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   |
   = help: use a custom `Error` type instead
   = help: for further information visit https://rust-lang.github.io/rust-clippy/rust-1.93.0/index.html#result_unit_err

Link: https://rust-lang.github.io/rust-clippy/rust-1.93.0/index.html#result_unit_err
Signed-off-by: Abe Kohandel <abe@kodebooth.com>

oxide-auth/src/primitives/issuer.rs
@@ -9,6 +9,7 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 
 use chrono::{Duration, Utc};
 
+use crate::OAuthOpaqueError;
 use crate::{endpoint::PreGrant, code_grant::accesstoken::BearerToken};
 use super::Time;
 use super::grant::Grant;
@@ -22,16 +23,16 @@ use super::generator::{TagGrant, TaggedAssertion, Assertion};
 /// they do not intend to offer a statefull refresh api).
 pub trait Issuer {
     /// Create a token authorizing the request parameters
-    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, ()>;
+    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, OAuthOpaqueError>;
 
     /// Refresh a token.
-    fn refresh(&mut self, _refresh: &str, _grant: Grant) -> Result<RefreshedToken, ()>;
+    fn refresh(&mut self, _refresh: &str, _grant: Grant) -> Result<RefreshedToken, OAuthOpaqueError>;
 
     /// Get the values corresponding to a bearer token
-    fn recover_token<'a>(&'a self, _: &'a str) -> Result<Option<Grant>, ()>;
+    fn recover_token<'a>(&'a self, _: &'a str) -> Result<Option<Grant>, OAuthOpaqueError>;
 
     /// Get the values corresponding to a refresh token
-    fn recover_refresh<'a>(&'a self, _: &'a str) -> Result<Option<Grant>, ()>;
+    fn recover_refresh<'a>(&'a self, _: &'a str) -> Result<Option<Grant>, OAuthOpaqueError>;
 }
 
 /// Token parameters returned to a client.
@@ -242,7 +243,7 @@ impl IssuedToken {
 }
 
 impl<G: TagGrant> Issuer for TokenMap<G> {
-    fn issue(&mut self, mut grant: Grant) -> Result<IssuedToken, ()> {
+    fn issue(&mut self, mut grant: Grant) -> Result<IssuedToken, OAuthOpaqueError> {
         self.set_duration(&mut grant);
         // The (usage, grant) tuple needs to be unique. Since this wraps after 2^63 operations, we
         // expect the validity time of the grant to have changed by then. This works when you don't
@@ -281,13 +282,13 @@ impl<G: TagGrant> Issuer for TokenMap<G> {
         })
     }
 
-    fn refresh(&mut self, refresh: &str, mut grant: Grant) -> Result<RefreshedToken, ()> {
+    fn refresh(&mut self, refresh: &str, mut grant: Grant) -> Result<RefreshedToken, OAuthOpaqueError> {
         // Remove the old token.
         let (refresh_key, mut token) = self
             .refresh
             .remove_entry(refresh)
             // Should only be called on valid refresh tokens.
-            .ok_or(())?;
+            .ok_or(OAuthOpaqueError)?;
 
         assert!(Arc::ptr_eq(token.refresh.as_ref().unwrap(), &refresh_key));
         self.set_duration(&mut grant);
@@ -328,11 +329,11 @@ impl<G: TagGrant> Issuer for TokenMap<G> {
         })
     }
 
-    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         Ok(self.access.get(token).map(|token| token.grant.clone()))
     }
 
-    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         Ok(self.refresh.get(token).map(|token| token.grant.clone()))
     }
 }
@@ -409,7 +410,7 @@ impl TokenSigner {
         self.counter.fetch_add(1, Ordering::Relaxed)
     }
 
-    fn refreshable_token(&self, grant: &Grant) -> Result<IssuedToken, ()> {
+    fn refreshable_token(&self, grant: &Grant) -> Result<IssuedToken, OAuthOpaqueError> {
         let first_ctr = self.next_counter() as u64;
         let second_ctr = self.next_counter() as u64;
 
@@ -424,7 +425,7 @@ impl TokenSigner {
         })
     }
 
-    fn unrefreshable_token(&self, grant: &Grant) -> Result<IssuedToken, ()> {
+    fn unrefreshable_token(&self, grant: &Grant) -> Result<IssuedToken, OAuthOpaqueError> {
         let counter = self.next_counter() as u64;
 
         let token = self.as_token().sign(counter, grant)?;
@@ -442,97 +443,97 @@ impl TokenSigner {
 }
 
 impl<I: Issuer + ?Sized> Issuer for &mut I {
-    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, ()> {
+    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, OAuthOpaqueError> {
         (**self).issue(grant)
     }
 
-    fn refresh(&mut self, token: &str, grant: Grant) -> Result<RefreshedToken, ()> {
+    fn refresh(&mut self, token: &str, grant: Grant) -> Result<RefreshedToken, OAuthOpaqueError> {
         (**self).refresh(token, grant)
     }
 
-    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         (**self).recover_token(token)
     }
 
-    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         (**self).recover_refresh(token)
     }
 }
 
 impl<I: Issuer + ?Sized> Issuer for Box<I> {
-    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, ()> {
+    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, OAuthOpaqueError> {
         (**self).issue(grant)
     }
 
-    fn refresh(&mut self, token: &str, grant: Grant) -> Result<RefreshedToken, ()> {
+    fn refresh(&mut self, token: &str, grant: Grant) -> Result<RefreshedToken, OAuthOpaqueError> {
         (**self).refresh(token, grant)
     }
 
-    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         (**self).recover_token(token)
     }
 
-    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         (**self).recover_refresh(token)
     }
 }
 
 impl<'s, I: Issuer + ?Sized> Issuer for MutexGuard<'s, I> {
-    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, ()> {
+    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, OAuthOpaqueError> {
         (**self).issue(grant)
     }
 
-    fn refresh(&mut self, token: &str, grant: Grant) -> Result<RefreshedToken, ()> {
+    fn refresh(&mut self, token: &str, grant: Grant) -> Result<RefreshedToken, OAuthOpaqueError> {
         (**self).refresh(token, grant)
     }
 
-    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         (**self).recover_token(token)
     }
 
-    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         (**self).recover_refresh(token)
     }
 }
 
 impl<'s, I: Issuer + ?Sized> Issuer for RwLockWriteGuard<'s, I> {
-    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, ()> {
+    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, OAuthOpaqueError> {
         (**self).issue(grant)
     }
 
-    fn refresh(&mut self, token: &str, grant: Grant) -> Result<RefreshedToken, ()> {
+    fn refresh(&mut self, token: &str, grant: Grant) -> Result<RefreshedToken, OAuthOpaqueError> {
         (**self).refresh(token, grant)
     }
 
-    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         (**self).recover_token(token)
     }
 
-    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         (**self).recover_refresh(token)
     }
 }
 
 impl Issuer for TokenSigner {
-    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, ()> {
+    fn issue(&mut self, grant: Grant) -> Result<IssuedToken, OAuthOpaqueError> {
         (&mut &*self).issue(grant)
     }
 
-    fn refresh(&mut self, _refresh: &str, _grant: Grant) -> Result<RefreshedToken, ()> {
-        Err(())
+    fn refresh(&mut self, _refresh: &str, _grant: Grant) -> Result<RefreshedToken, OAuthOpaqueError> {
+        Err(OAuthOpaqueError)
     }
 
-    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_token<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         (&self).recover_token(token)
     }
 
-    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, ()> {
+    fn recover_refresh<'a>(&'a self, token: &'a str) -> Result<Option<Grant>, OAuthOpaqueError> {
         (&self).recover_refresh(token)
     }
 }
 
 impl Issuer for &TokenSigner {
-    fn issue(&mut self, mut grant: Grant) -> Result<IssuedToken, ()> {
+    fn issue(&mut self, mut grant: Grant) -> Result<IssuedToken, OAuthOpaqueError> {
         if let Some(duration) = &self.duration {
             grant.until = Utc::now() + *duration;
         }
@@ -544,15 +545,15 @@ impl Issuer for &TokenSigner {
         }
     }
 
-    fn refresh(&mut self, _refresh: &str, _grant: Grant) -> Result<RefreshedToken, ()> {
-        Err(())
+    fn refresh(&mut self, _refresh: &str, _grant: Grant) -> Result<RefreshedToken, OAuthOpaqueError> {
+        Err(OAuthOpaqueError)
     }
 
-    fn recover_token<'t>(&'t self, token: &'t str) -> Result<Option<Grant>, ()> {
+    fn recover_token<'t>(&'t self, token: &'t str) -> Result<Option<Grant>, OAuthOpaqueError> {
         Ok(self.as_token().extract(token).ok())
     }
 
-    fn recover_refresh<'t>(&'t self, token: &'t str) -> Result<Option<Grant>, ()> {
+    fn recover_refresh<'t>(&'t self, token: &'t str) -> Result<Option<Grant>, OAuthOpaqueError> {
         if !self.have_refresh {
             return Ok(None);
         }
@@ -663,7 +664,7 @@ pub mod tests {
     fn bad_generator() {
         struct BadGenerator;
         impl TagGrant for BadGenerator {
-            fn tag(&mut self, _: u64, _: &Grant) -> Result<String, ()> {
+            fn tag(&mut self, _: u64, _: &Grant) -> Result<String, OAuthOpaqueError> {
                 Ok("YOLO.HowBadCanItBeToRepeatTokens?".into())
             }
         }

oxide-auth-async/src/code_grant.rs
@@ -1,4 +1,5 @@
 pub mod refresh {
+    use oxide_auth::OAuthOpaqueError;
     use oxide_auth::code_grant::refresh::{BearerToken, Error, Input, Output, Refresh, Request};
     use oxide_auth::primitives::{grant::Grant, registrar::RegistrarError};
 
@@ -29,7 +30,7 @@ pub mod refresh {
                         .issuer()
                         .refresh(&token, *grant)
                         .await
-                        .map_err(|()| Error::Primitive)?;
+                        .map_err(|OAuthOpaqueError| Error::Primitive)?;
                     Input::Refreshed(refreshed)
                 }
                 Requested::RecoverRefresh { token } => {
@@ -37,7 +38,7 @@ pub mod refresh {
                         .issuer()
                         .recover_refresh(&token)
                         .await
-                        .map_err(|()| Error::Primitive)?;
+                        .map_err(|OAuthOpaqueError| Error::Primitive)?;
                     Input::Recovered {
                         scope: request.scope(),
                         grant: recovered.map(Box::new),
@@ -134,8 +135,9 @@ pub mod client_credentials {
     use async_trait::async_trait;
     use chrono::{Utc, Duration};
     use oxide_auth::{
+        OAuthOpaqueError,
         code_grant::{
-            accesstoken::{PrimitiveError, BearerToken},
+            accesstoken::{BearerToken, PrimitiveError},
             client_credentials::{ClientCredentials, Error, Input, Output, Request as TokenRequest},
         },
         endpoint::{PreGrant, Scope, Solicitation},
@@ -221,7 +223,7 @@ pub mod client_credentials {
                     extensions: self.extensions,
                 })
                 .await
-                .map_err(|()| Error::Primitive(Box::new(PrimitiveError::empty())))?;
+                .map_err(|OAuthOpaqueError| Error::Primitive(Box::new(PrimitiveError::empty())))?;
 
             if !allow_refresh_token {
                 token.refresh = None;
@@ -480,6 +482,7 @@ pub mod authorization {
     use async_trait::async_trait;
     use chrono::{Duration, Utc};
     use oxide_auth::{
+        OAuthOpaqueError,
         code_grant::{
             authorization::{Authorization, Error, ErrorUrl, Input, Output, Request},
             error::{AuthorizationError, AuthorizationErrorType},
@@ -579,7 +582,7 @@ pub mod authorization {
                     extensions: self.extensions,
                 })
                 .await
-                .map_err(|()| Error::PrimitiveError)?;
+                .map_err(|OAuthOpaqueError| Error::PrimitiveError)?;
 
             url.query_pairs_mut()
                 .append_pair("code", grant.as_str())

oxide-auth-async/src/primitives.rs
@@ -1,5 +1,6 @@
 //! Async versions of all primitives traits.
 use async_trait::async_trait;
+use oxide_auth::OAuthOpaqueError;
 use oxide_auth::primitives::{grant::Grant, scope::Scope};
 use oxide_auth::primitives::issuer::{IssuedToken, RefreshedToken};
 use oxide_auth::primitives::{
@@ -9,9 +10,9 @@ use oxide_auth::primitives::{
 
 #[async_trait]
 pub trait Authorizer {
-    async fn authorize(&mut self, _: Grant) -> Result<String, ()>;
+    async fn authorize(&mut self, _: Grant) -> Result<String, OAuthOpaqueError>;
 
-    async fn extract(&mut self, _: &str) -> Result<Option<Grant>, ()>;
+    async fn extract(&mut self, _: &str) -> Result<Option<Grant>, OAuthOpaqueError>;
 }
 
 #[async_trait]
@@ -19,24 +20,24 @@ impl<T> Authorizer for T
 where
     T: authorizer::Authorizer + Send + ?Sized,
 {
-    async fn authorize(&mut self, grant: Grant) -> Result<String, ()> {
+    async fn authorize(&mut self, grant: Grant) -> Result<String, OAuthOpaqueError> {
         authorizer::Authorizer::authorize(self, grant)
     }
 
-    async fn extract(&mut self, token: &str) -> Result<Option<Grant>, ()> {
+    async fn extract(&mut self, token: &str) -> Result<Option<Grant>, OAuthOpaqueError> {
         authorizer::Authorizer::extract(self, token)
     }
 }
 
 #[async_trait]
 pub trait Issuer {
-    async fn issue(&mut self, _: Grant) -> Result<IssuedToken, ()>;
+    async fn issue(&mut self, _: Grant) -> Result<IssuedToken, OAuthOpaqueError>;
 
-    async fn refresh(&mut self, _: &str, _: Grant) -> Result<RefreshedToken, ()>;
+    async fn refresh(&mut self, _: &str, _: Grant) -> Result<RefreshedToken, OAuthOpaqueError>;
 
-    async fn recover_token(&mut self, _: &str) -> Result<Option<Grant>, ()>;
+    async fn recover_token(&mut self, _: &str) -> Result<Option<Grant>, OAuthOpaqueError>;
 
-    async fn recover_refresh(&mut self, _: &str) -> Result<Option<Grant>, ()>;
+    async fn recover_refresh(&mut self, _: &str) -> Result<Option<Grant>, OAuthOpaqueError>;
 }
 
 #[async_trait]
@@ -44,19 +45,19 @@ impl<T> Issuer for T
 where
     T: issuer::Issuer + Send + ?Sized,
 {
-    async fn issue(&mut self, grant: Grant) -> Result<IssuedToken, ()> {
+    async fn issue(&mut self, grant: Grant) -> Result<IssuedToken, OAuthOpaqueError> {
         issuer::Issuer::issue(self, grant)
     }
 
-    async fn refresh(&mut self, token: &str, grant: Grant) -> Result<RefreshedToken, ()> {
+    async fn refresh(&mut self, token: &str, grant: Grant) -> Result<RefreshedToken, OAuthOpaqueError> {
         issuer::Issuer::refresh(self, token, grant)
     }
 
-    async fn recover_token(&mut self, token: &str) -> Result<Option<Grant>, ()> {
+    async fn recover_token(&mut self, token: &str) -> Result<Option<Grant>, OAuthOpaqueError> {
         issuer::Issuer::recover_token(self, token)
     }
 
-    async fn recover_refresh(&mut self, token: &str) -> Result<Option<Grant>, ()> {
+    async fn recover_refresh(&mut self, token: &str) -> Result<Option<Grant>, OAuthOpaqueError> {
         issuer::Issuer::recover_refresh(self, token)
     }
 }

oxide-auth-async/src/tests/mod.rs
@@ -2,9 +2,9 @@ use std::borrow::Cow;
 use std::collections::HashMap;
 
 use oxide_auth::{
-    primitives::generator::TagGrant,
-    endpoint::{WebRequest, WebResponse, OwnerConsent, QueryParameter, Solicitation},
-    primitives::grant::Grant,
+    OAuthOpaqueError,
+    endpoint::{OwnerConsent, QueryParameter, Solicitation, WebRequest, WebResponse},
+    primitives::{generator::TagGrant, grant::Grant},
 };
 use url::Url;
 
@@ -150,7 +150,7 @@ impl WebResponse for CraftedResponse {
 struct TestGenerator(String);
 
 impl TagGrant for TestGenerator {
-    fn tag(&mut self, _: u64, _grant: &Grant) -> Result<String, ()> {
+    fn tag(&mut self, _: u64, _grant: &Grant) -> Result<String, OAuthOpaqueError> {
         Ok(self.0.clone())
     }
 }

oxide-auth/src/code_grant/accesstoken.rs
@@ -7,6 +7,7 @@ use chrono::Utc;
 use serde::{Deserialize, Serialize};
 use serde_json;
 
+use crate::OAuthOpaqueError;
 use crate::code_grant::error::{AccessTokenError, AccessTokenErrorType};
 use crate::primitives::authorizer::Authorizer;
 use crate::primitives::issuer::{IssuedToken, Issuer};
@@ -99,12 +100,15 @@ pub trait Extension {
     ///
     /// The input data comes from the extension data produced in the handling of the
     /// authorization code request.
-    fn extend(&mut self, request: &dyn Request, data: Extensions)
-        -> std::result::Result<Extensions, ()>;
+    fn extend(
+        &mut self, request: &dyn Request, data: Extensions,
+    ) -> std::result::Result<Extensions, OAuthOpaqueError>;
 }
 
 impl Extension for () {
-    fn extend(&mut self, _: &dyn Request, _: Extensions) -> std::result::Result<Extensions, ()> {
+    fn extend(
+        &mut self, _: &dyn Request, _: Extensions,
+    ) -> std::result::Result<Extensions, OAuthOpaqueError> {
         Ok(Extensions::new())
     }
 }

oxide-auth/src/code_grant/authorization.rs
@@ -5,6 +5,7 @@ use std::result::Result as StdResult;
 use url::Url;
 use chrono::{Duration, Utc};
 
+use crate::OAuthOpaqueError;
 use crate::code_grant::error::{AuthorizationError, AuthorizationErrorType};
 use crate::primitives::authorizer::Authorizer;
 use crate::primitives::registrar::{ClientUrl, ExactUrl, Registrar, RegistrarError, PreGrant};
@@ -43,11 +44,11 @@ pub trait Request {
 /// An endpoint not having any extension may use `&mut ()` as the result of system.
 pub trait Extension {
     /// Inspect the request to produce extension data.
-    fn extend(&mut self, request: &dyn Request) -> std::result::Result<Extensions, ()>;
+    fn extend(&mut self, request: &dyn Request) -> std::result::Result<Extensions, OAuthOpaqueError>;
 }
 
 impl Extension for () {
-    fn extend(&mut self, _: &dyn Request) -> std::result::Result<Extensions, ()> {
+    fn extend(&mut self, _: &dyn Request) -> std::result::Result<Extensions, OAuthOpaqueError> {
         Ok(Extensions::new())
     }
 }
@@ -353,7 +354,7 @@ pub fn authorization_code(handler: &mut dyn Endpoint, request: &dyn Request) -> 
             Requested::Extend => {
                 let grant_extension = match handler.extension().extend(request) {
                     Ok(extension_data) => extension_data,
-                    Err(()) => {
+                    Err(OAuthOpaqueError) => {
                         let prepared_error = ErrorUrl::with_request(
                             request,
                             the_redirect_uri.unwrap().into(),
@@ -470,7 +471,7 @@ impl Pending {
                 until: Utc::now() + Duration::minutes(10),
                 extensions: self.extensions,
             })
-            .map_err(|()| Error::PrimitiveError)?;
+            .map_err(|OAuthOpaqueError| Error::PrimitiveError)?;
 
         url.query_pairs_mut()
             .append_pair("code", grant.as_str())

oxide-auth/src/code_grant/client_credentials.rs
@@ -4,6 +4,7 @@ use std::borrow::Cow;
 
 use chrono::{Utc, Duration};
 
+use crate::OAuthOpaqueError;
 use crate::code_grant::accesstoken::BearerToken;
 use crate::code_grant::error::{AccessTokenError, AccessTokenErrorType};
 use crate::endpoint::{Scope, Solicitation};
@@ -64,11 +65,11 @@ pub trait Request {
 /// An endpoint not having any extension may use `&mut ()` as the result of system.
 pub trait Extension {
     /// Inspect the request to produce extension data.
-    fn extend(&mut self, request: &dyn Request) -> std::result::Result<Extensions, ()>;
+    fn extend(&mut self, request: &dyn Request) -> std::result::Result<Extensions, OAuthOpaqueError>;
 }
 
 impl Extension for () {
-    fn extend(&mut self, _: &dyn Request) -> std::result::Result<Extensions, ()> {
+    fn extend(&mut self, _: &dyn Request) -> std::result::Result<Extensions, OAuthOpaqueError> {
         Ok(Extensions::new())
     }
 }
@@ -408,7 +409,7 @@ impl Pending {
                 until: Utc::now() + Duration::minutes(10),
                 extensions: self.extensions,
             })
-            .map_err(|()| Error::Primitive(Box::new(PrimitiveError::empty())))?;
+            .map_err(|OAuthOpaqueError| Error::Primitive(Box::new(PrimitiveError::empty())))?;
 
         if !allow_refresh_token {
             token.refresh = None;

oxide-auth/src/code_grant/extensions/pkce.rs
@@ -1,6 +1,9 @@
 use std::borrow::Cow;
 
-use crate::primitives::grant::{GrantExtension, Value};
+use crate::{
+    OAuthOpaqueError,
+    primitives::grant::{GrantExtension, Value},
+};
 
 use base64::{self, engine::general_purpose::URL_SAFE_NO_PAD, Engine};
 use sha2::{Digest, Sha256};
@@ -73,11 +76,11 @@ impl Pkce {
     /// a SHA256 methods the results would not be quite as severe but still bad practice.
     pub fn challenge(
         &self, method: Option<Cow<str>>, challenge: Option<Cow<str>>,
-    ) -> Result<Option<Value>, ()> {
+    ) -> Result<Option<Value>, OAuthOpaqueError> {
         let method = method.unwrap_or(Cow::Borrowed("plain"));
 
         let challenge = match challenge {
-            None if self.required => return Err(()),
+            None if self.required => return Err(OAuthOpaqueError),
             None => return Ok(None),
             Some(challenge) => challenge,
         };
@@ -96,18 +99,20 @@ impl Pkce {
     ///
     /// When a challenge was agreed upon but no verifier is present, this method will return an
     /// error.
-    pub fn verify(&self, method: Option<Value>, verifier: Option<Cow<str>>) -> Result<(), ()> {
+    pub fn verify(
+        &self, method: Option<Value>, verifier: Option<Cow<str>>,
+    ) -> Result<(), OAuthOpaqueError> {
         let (method, verifier) = match (method, verifier) {
-            (None, _) if self.required => return Err(()),
+            (None, _) if self.required => return Err(OAuthOpaqueError),
             (None, _) => return Ok(()),
             // An internal saved method but no verifier
-            (Some(_), None) => return Err(()),
+            (Some(_), None) => return Err(OAuthOpaqueError),
             (Some(method), Some(verifier)) => (method, verifier),
         };
 
         let method = match method.into_private_value() {
             Ok(Some(method)) => method,
-            _ => return Err(()),
+            _ => return Err(OAuthOpaqueError),
         };
 
         let method = Method::from_encoded(Cow::Owned(method))?;
@@ -128,19 +133,19 @@ fn b64encode(data: &[u8]) -> String {
 }
 
 impl Method {
-    fn from_parameter(method: Cow<str>, challenge: Cow<str>) -> Result<Self, ()> {
+    fn from_parameter(method: Cow<str>, challenge: Cow<str>) -> Result<Self, OAuthOpaqueError> {
         match method.as_ref() {
             "plain" => Ok(Method::Plain(challenge.into_owned())),
             "S256" => Ok(Method::Sha256(challenge.into_owned())),
-            _ => Err(()),
+            _ => Err(OAuthOpaqueError),
         }
     }
 
-    fn assert_supported_method(self, allow_plain: bool) -> Result<Self, ()> {
+    fn assert_supported_method(self, allow_plain: bool) -> Result<Self, OAuthOpaqueError> {
         match (self, allow_plain) {
             (this, true) => Ok(this),
             (Method::Sha256(content), false) => Ok(Method::Sha256(content)),
-            (Method::Plain(_), false) => Err(()),
+            (Method::Plain(_), false) => Err(OAuthOpaqueError),
         }
     }
 
@@ -151,24 +156,24 @@ impl Method {
         }
     }
 
-    fn from_encoded(encoded: Cow<str>) -> Result<Method, ()> {
+    fn from_encoded(encoded: Cow<str>) -> Result<Method, OAuthOpaqueError> {
         // TODO: avoid allocation in case of borrow and invalid.
         let mut encoded = encoded.into_owned();
         match encoded.pop() {
-            None => Err(()),
+            None => Err(OAuthOpaqueError),
             Some('p') => Ok(Method::Plain(encoded)),
             Some('S') => Ok(Method::Sha256(encoded)),
-            _ => Err(()),
+            _ => Err(OAuthOpaqueError),
         }
     }
 
-    fn verify(&self, verifier: &str) -> Result<(), ()> {
+    fn verify(&self, verifier: &str) -> Result<(), OAuthOpaqueError> {
         match self {
             Method::Plain(encoded) => {
                 if encoded.as_bytes().ct_eq(verifier.as_bytes()).into() {
                     Ok(())
                 } else {
-                    Err(())
+                    Err(OAuthOpaqueError)
                 }
             }
             Method::Sha256(encoded) => {
@@ -178,7 +183,7 @@ impl Method {
                 if encoded.as_bytes().ct_eq(b64digest.as_bytes()).into() {
                     Ok(())
                 } else {
-                    Err(())
+                    Err(OAuthOpaqueError)
                 }
             }
         }

commit 3cfc97c5f0f8cd0d52f7502c74f114dad5e73d77

test(oxide-auth): fix doc tests for oxide-auth crate
As part of the changes in #235, doc tests for oxide-auth crate were left
out. This change addresses those errors.

Fixes: 71dd0751f92c ("clippy: resolve result_unit_err")
Signed-off-by: Abe Kohandel <abe@kodebooth.com>

oxide-auth/src/primitives/issuer.rs
@@ -199,6 +199,7 @@ impl IssuedToken {
     /// # use oxide_auth::primitives::issuer::RefreshedToken;
     /// use oxide_auth::primitives::grant::Grant;
     /// use oxide_auth::primitives::issuer::{Issuer, IssuedToken};
+    /// use oxide_auth::OAuthOpaqueError;
     ///
     /// struct MyIssuer;
     ///
@@ -210,14 +211,14 @@ impl IssuedToken {
     /// }
     ///
     /// impl Issuer for MyIssuer {
-    ///     fn issue(&mut self, mut grant: Grant) -> Result<IssuedToken, ()> {
+    ///     fn issue(&mut self, mut grant: Grant) -> Result<IssuedToken, OAuthOpaqueError> {
     ///         let token = self.access_token(&grant);
     ///         Ok(IssuedToken::without_refresh(token, grant.until))
     ///     }
     ///     // …
-    /// # fn recover_token<'t>(&'t self, token: &'t str) -> Result<Option<Grant>, ()> { Err(()) }
-    /// # fn recover_refresh<'t>(&'t self, token: &'t str) -> Result<Option<Grant>, ()> { Err(()) }
-    /// # fn refresh(&mut self, _: &str, _: Grant) -> Result<RefreshedToken, ()> { Err(()) }
+    /// # fn recover_token<'t>(&'t self, token: &'t str) -> Result<Option<Grant>, OAuthOpaqueError> { Err(OAuthOpaqueError) }
+    /// # fn recover_refresh<'t>(&'t self, token: &'t str) -> Result<Option<Grant>, OAuthOpaqueError> { Err(OAuthOpaqueError) }
+    /// # fn refresh(&mut self, _: &str, _: Grant) -> Result<RefreshedToken, OAuthOpaqueError> { Err(OAuthOpaqueError) }
     /// }
     /// ```
     pub fn without_refresh(token: String, until: Time) -> Self {